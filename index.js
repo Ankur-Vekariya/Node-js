ffa160341b77146ea8cd09645e97936a550830eb
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-08-27 20:01:40 +0530

    Render seprate HTML file

--- index.js
+++ index.js
@@ -144,3 +144,15 @@
 // });
 
 // app.listen(5000);
+
+// Render sepraate HTML file
+
+// const express = require("express");
+// const path = require("path");
+
+// const app = express();
+// const publicPath = path.join(__dirname, "public");
+
+// app.use(express.static(publicPath));
+
+// app.listen(5000);
