8b03a6295fa0e24313d81b1e364c261f8a39c88d
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-08-27 19:35:27 +0530

    Basic express

--- index.js
+++ index.js
@@ -101,7 +101,18 @@
 
 // express js
 
+// const express = require('express');
+// const app = express();
 
+// app.get('',(req,res)=>{
+//     res.send("Welcome,this is home page")
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.send("Hello,this is about page")
+// })
+
+// app.listen(5000);
 
 
 
