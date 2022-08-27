dbcf3fc1894921467fc8268745be134d6e05af51
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-08-27 18:57:05 +0530

    how node js works

--- index.js
+++ index.js
@@ -79,3 +79,21 @@
 // wait.then((data) => {
 //   console.log(a + data);
 // });
+
+//  How node js works 
+// event loop
+// call stack
+// Node api 
+// callback queue
+
+console.log("starting exe...");
+
+setTimeout(() => {
+  console.log("2 second log");
+}, 2000);
+
+setTimeout(() => {
+  console.log("0 second log");
+}, 0);
+
+console.log("finishing exe....");
