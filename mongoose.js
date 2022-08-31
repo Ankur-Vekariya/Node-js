55d20b3bc3341a76c289cf4cdc14481b87cf9189
ankur12v <65348445+ankur12v@users.noreply.github.com>
2022-08-31 21:08:41 +0530

    Mongoose for database connection

---
+++ mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const main = async () => {
+  await mongoose.connect("mongodb://localhost:27017/e-comm");
+  const ProductSch = new mongoose.Schema({
+    name: String,
+  });
+  const ProductsModal = mongoose.model("product", ProductSch);
+  let data = new ProductsModal({ name: "m8" });
+  let result = await data.save();
+  console.log(result);
+};
+main()
